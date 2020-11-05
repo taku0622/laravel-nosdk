0a216066badcfe338e8cf3b75f4ae938ed0ed1f1
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-05 18:48:52 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -13,5 +13,14 @@ class LineBotController extends Controller
     public function response(Request $request)
     {
         error_log("hello...");
+
+        $input = file_get_contents('php://input');
+        if (!empty($input)) {
+            $events = json_decode($input)->events;
+            foreach ($events as $event) {
+                error_log(json_encode($event, JSON_UNESCAPED_UNICODE));
+                // bot($event);
+            }
+        }
     }
 }
