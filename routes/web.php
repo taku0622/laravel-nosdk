dfbf34ce77c386d93bb9f90b55947e6a3c5d0d92
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-05 18:38:47 +0900

    done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LineBotController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [LineBotController::class, 'index']);
+// Route::get('/hello', 'LineBotController@index');
