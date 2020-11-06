5dd895bcd6be68e44cfa2929bd1bf83fbd2807a5
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-06 14:19:18 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ResponseController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
+    public function response()
+    {
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LineBotController;
+use App\Http\Controllers\ResponseController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +22,5 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', [LineBotController::class, 'index']);
+Route::get('/res', [ResponseController::class, 'index']);
 // Route::get('/hello', 'LineBotController@index');
