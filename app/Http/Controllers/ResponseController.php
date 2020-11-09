949df6acf3ebc80f5d7664d201d99b3a27cc40a7
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-09 19:08:05 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -13,11 +13,13 @@ class ResponseController extends Controller
         $events = json_decode($request->getContent(), true);
         foreach ($events as $event) {
             error_log("################################## event is ##################################");
-            error_log(json_encode($event, JSON_UNESCAPED_UNICODE));
+            // error_log(json_encode($event, JSON_UNESCAPED_UNICODE));
             $usersId = $event["to"]; // array
             $type = $event["type"];
             $text = $event["text"];
             error_log("usersId: " . implode(",", $usersId) . "  type: " . $type . "  text: " . $text);
+
+            return error_log(json_encode($event, JSON_UNESCAPED_UNICODE));
         }
     }
 }
