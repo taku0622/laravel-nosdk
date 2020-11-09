8368149d68b0bdf0bafc0f7bf3af7b62f4fceee2
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-09 16:31:46 +0900

    done

---
+++ database/migrations/2020_11_09_072123_create_conversations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->text('userid');
+            $table->text('conversation_id');
+            $table->text('dialog_node');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
