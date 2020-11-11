751c3369a52cb034d0bc51cf63ba3818458a41dc
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-11 21:53:58 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -209,6 +209,9 @@ class ResponseController extends Controller
     }
     public function res(Request $request)
     {
+        error_log("################################## request is ##################################");
+        error_log($request->getContent());
+
         $events = json_decode($request->getContent(), true);
         foreach ($events as $event) {
             error_log("################################## event is ##################################");
