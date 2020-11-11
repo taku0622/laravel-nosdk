ef1f56f45ebe23f0d4ae3580f20f7e908c912c08
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-11 21:42:32 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -256,7 +256,6 @@ class ResponseController extends Controller
         //     error_log(gettype(json_encode($message, JSON_UNESCAPED_UNICODE)));
         //     return json_encode($message, JSON_UNESCAPED_UNICODE);
         return "ok";
-            // echo json_encode($message, JSON_UNESCAPED_UNICODE);
-        }
+        // echo json_encode($message, JSON_UNESCAPED_UNICODE);
     }
 }
