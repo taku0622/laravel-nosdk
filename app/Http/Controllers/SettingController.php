9c95a26192acae109a3885df13dd1a2a84867902
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-14 21:51:25 +0900

    done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -9,6 +9,14 @@ class SettingController extends Controller
     public function index($id)
     {
         error_log("hello...");
-        error_log($id);
+        error_log("id: " . $id);
+    }
+
+    public function update(Request $request)
+    {
+        error_log("hello...");
+        error_log($request->getContent());
+        $input = $request->getContent();
+        error_log("input: " . $input);
     }
 }
