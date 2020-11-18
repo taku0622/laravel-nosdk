3b55a8b99fb8501b4bbe0e11d2e404746185cd76
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-18 18:03:25 +0900

    done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -14,14 +14,12 @@ class SettingController extends Controller
         error_log("id: " . $id);
         #################################################################
         $student = DB::table('students')->where('user_id', $id)->first();
-        // $student = DB::table('students')->first();
-        error_log(json_encode($student, JSON_UNESCAPED_UNICODE));
+        // error_log(json_encode($student, JSON_UNESCAPED_UNICODE));
         // if ($student->isEmpty()) {
         //     error_log("データがありません");
         // } else {
         //     error_log($student->number);
         // }
-        error_log($student->number);
         $profile = [
             "number" => $student->number,
             "push_new" => $student->push_new,
@@ -29,15 +27,6 @@ class SettingController extends Controller
             "push_cancel" => $student->push_cancel,
             "push_event" => $student->push_event,
         ];
-        #################################################################
-        // $profile = [
-        //     "number" => "C0117253",
-        //     "push_new" => true,
-        //     "push_important" => false,
-        //     "push_cancel" => true,
-        //     "push_event" => false,
-        // ];
-        error_log(json_encode($profile));
         error_log(gettype(json_encode($profile)));
         return json_encode($profile);
     }
