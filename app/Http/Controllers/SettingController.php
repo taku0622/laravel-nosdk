88dbb17985790ac26da9a45cb4699ef584ff8a70
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-18 17:45:26 +0900

    done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -13,28 +13,29 @@ class SettingController extends Controller
         error_log("hello...");
         error_log("id: " . $id);
         #################################################################
-        // $student = DB::table('students')->where('user_id', $id)->get();
-        // if ($student->isEmpty()) {
-        //     error_log("データがありません");
-        // } else {
-        //     error_log($student->number);
-        // }
-        // error_log($student->number);
-        // $profile = [
-        //     "number" => $student->number,
-        //     "push_new" => $student->push_new,
-        //     "push_important" => $student->push_important,
-        //     "push_cancel" => $student->push_cancel,
-        //     "push_event" => $student->push_event,
-        // ];
-        #################################################################
+        $student = DB::table('students')->where('user_id', $id)->get();
+        error_log(json_encode($student, JSON_UNESCAPED_UNICODE));
+        if ($student->isEmpty()) {
+            error_log("データがありません");
+        } else {
+            error_log($student->number);
+        }
+        error_log($student->number);
         $profile = [
-            "number" => "C0117253",
-            "push_new" => true,
-            "push_important" => false,
-            "push_cancel" => true,
-            "push_event" => false,
+            "number" => $student->number,
+            "push_new" => $student->push_new,
+            "push_important" => $student->push_important,
+            "push_cancel" => $student->push_cancel,
+            "push_event" => $student->push_event,
         ];
+        #################################################################
+        // $profile = [
+        //     "number" => "C0117253",
+        //     "push_new" => true,
+        //     "push_important" => false,
+        //     "push_cancel" => true,
+        //     "push_event" => false,
+        // ];
         error_log(json_encode($profile));
         error_log(gettype(json_encode($profile)));
         return json_encode($profile);
