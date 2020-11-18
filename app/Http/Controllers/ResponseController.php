0d54876ae15e88f3b1e6d9fdc6b6bbc310a284da
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-18 18:57:59 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -219,15 +219,17 @@ class ResponseController extends Controller
 
     public function followEvent($userId)
     {
-        // DB::table('students')->insert(
-        //     [
-        //         'user_id' => $userId,
-        //         'push_new' => TRUE,
-        //         'push_important' => TRUE,
-        //         'push_cancel' => TRUE,
-        //         'push_event' => TRUE,
-        //     ]
-        // );
+        DB::table('students')->insert(
+            [
+                'user_id' => $userId,
+                'number' => "",
+                'department' => "",
+                'push_new' => TRUE,
+                'push_important' => TRUE,
+                'push_cancel' => TRUE,
+                'push_event' => TRUE,
+            ]
+        );
         $message = "followed";
         return $message;
     }
