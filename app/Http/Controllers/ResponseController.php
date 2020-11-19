6e94310063e9cd62e6b055add9b528e03461d829
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-19 15:39:54 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -159,14 +159,13 @@ class ResponseController extends Controller
     {
         $student = DB::table('students')->where('user_id', $userId)->first();
         error_log($student->department);
-        #############################################################
         $cancelInfomations = DB::table('cancel_informations')->where('department', $student->department)->get();
         if ($cancelInfomations->isEmpty()) {
             // $message = "あなたの学部の休講案内はありません";
             $message = [
                 "to" => [$userId],
                 "type" => "text",
-                "text" => "あなたの学部の休講案内はありません",
+                "text" => $student->department . "学部の休講案内はありません",
             ];
         } else {
             $contents = [];
@@ -188,33 +187,7 @@ class ResponseController extends Controller
                 "contents" => $contents
             ];
         }
-        #############################################################
-        // $contents = [
-        //     [
-        // 'title' => '2020年10月22日 3時限　並列・分散処理',
-        // 'content' => 'コンピュータサイエンス学部',
-        // 'uri' => 'https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/',
-        // 'label' => '詳細'
-        //     ],
-        //     [
-        //         'title' => '2020年10月27日 1,2,3時限　スポーツ実技Ⅳ',
-        //         'content' => 'コンディショニングエクササイズ　工学部(学科共通), メディア学部, コンピュータサイエンス学部, 応用生物学部',
-        //         'uri' => 'https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/',
-        //         'label' => '詳細'
-        //     ],
-        //     [
-        //         'title' => '2020年11月06日 1時限　知的財産権',
-        //         'content' => '工学部(学科共通)',
-        //         'uri' => 'https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/',
-        //         'label' => '詳細'
-        //     ]
-        // ];
-        // $message = [
-        //     "to" => [$userId],
-        //     "type" => "multiple",
-        //     "altText" =>  $text,
-        //     "contents" => $contents
-        // ];
+
         return $message;
     }
 
