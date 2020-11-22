167d7cf709647eaeb5ff41ca548e00e389cbec4d
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-22 23:08:27 +0900

    done

--- app/Http/Controllers/DataBaseController.php
+++ app/Http/Controllers/DataBaseController.php
@@ -26,21 +26,56 @@ class DataBaseController extends Controller
         error_log(gettype($inputs));
         $inputs = json_decode($inputs, true);
         foreach ($inputs as $input) {
+            error_log("input[day]: " . $input["day"]); // 1
+            error_log("input[name]: " . $input["name"]); // 2
+            error_log("input[instructor]: " . $input["instructor"]); // 3
+            error_log("input[department]: " . $input["department"]); // 4
+            error_log("input[grade]: " . $input["grade"]); // 5
+            error_log("input[class]: " . $input["class"]); // 5
+            error_log("input[note]: " . $input["note"]); // 6
+            error_log("input[up]: " . $input["up"]); // 7
+            error_log("input[from]: " . $input["from"]); // 8
+            error_log("データベースに入れるデータに変換###################################");
             // データ整形
             $date = mb_substr($input["day"], 0, 10); // 2020年11月29
-            $period = mb_substr($input["day"], 12);
-            $search = ['年', '月'];
-            $date = str_replace($search, '-', $date);
-            error_log("input[day]: " . $input["day"]);
-            error_log("date:" . $date);
-            error_log("period:" . $period);
-            error_log("input[name]: " . $input["name"]);
-            error_log("input[instructor]: " . $input["instructor"]);
-            error_log("input[department]: " . $input["department"]);
-            error_log("input[grade]: " . $input["grade"]);
-            error_log("input[note]: " . $input["note"]);
-            error_log("input[up]: " . $input["up"]);
-            error_log("input[from]: " . $input["from"]);
+            $period = mb_substr($input["day"], 12); // 3限目
+            $search = ['年', '月']; //置換する文字
+            $date = str_replace($search, '-', $date); //置換
+            # 学部
+            switch ($input["department"]) {
+                case '応用生物学部':
+                    $department = 'bs';
+                    break;
+                case 'コンピュータサイエンス学部':
+                    $department = 'cs';
+                    break;
+                case 'メディア学部':
+                    $department = 'ms';
+                    break;
+                case '工学部':
+                case '機械工学科':
+                    $department = 'es';
+                    break;
+                case 'デザイン学部':
+                    $department = 'ds';
+                    break;
+                case '医療保健学部':
+                    $department = 'hs';
+                    break;
+            }
+            $posted_date = mb_substr($input["up"], 0, 10); // 2020年11月29
+            $posted_date = str_replace($search, '-', $posted_date); //置換
+            error_log("date:" . $date); // 1
+            error_log("period:" . $period); // 2
+            error_log("lecture_name:" . $input["name"]); // 3
+            error_log("teacher_name:" . $input["instructor"]); // 4
+            error_log("grade:" . $input["grade"]); // 5
+            error_log("department:" . $department); // 6
+            error_log("class:" . $input["class"]); // 7
+            error_log("note: " . $input["note"]); // 8
+            error_log("posted_date: " . $posted_date); // 9
+            error_log("contributor: " . $input["from"]); // 10
+
         }
         return "connected!! updateCancel";
     }
