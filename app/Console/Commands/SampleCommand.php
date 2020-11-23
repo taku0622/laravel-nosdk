e2f8152e5f458c4f70f8950658876be4b654556c
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-24 00:43:16 +0900

    done

--- app/Console/Commands/SampleCommand.php
+++ app/Console/Commands/SampleCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Command;
 
 class SampleCommand extends Command
@@ -38,46 +39,53 @@ class SampleCommand extends Command
     public function handle()
     {
         // php artisan command:sample
-        $userId = "U6e0f4008a090ff5b5bef0323cae3428e";
-        $contents = [
-            [
-                'title' => '【図書館】リクエストの結果報告＜八王子キャンパス＞',
-                'content' => '10月（前半）の選書の結果、以下のリクエストが採択されました。',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/64555/',
-                'label' => '詳細'
-            ],
-            [
-                'title' => '2020年度第2学期（後期）放送大学特別聴講学生',
-                'content' => '放送大学特別聴講学生へ',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020_dai2gakki_housoudaigaku_1022.pdf',
-                'label' => '詳細'
-            ],
-            [
-                'title' => '【CS学部】2020年度「創成課題」教室（10/22更新）',
-                'content' => '属された研究室ごとに、創成課題を行います。',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020CS_souseikadai_kyousitu20201022.pdf',
-                'label' => '詳細'
-            ],
-            [
-                'title' => 'シェアサイクル設置のお知らせ（八王子キャンパス）',
-                'content' => '八王子キャンパスにシェアサイクルを設置することになりました。',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/shearingu_settiosirase_1021.pdf',
-                'label' => '詳細'
-            ],
-            [
-                'title' => '【図書館】図書館アルバイトを募集します！＜八王子キャンパス＞',
-                'content' => 'お申し込みを お待ちしています。',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/12658/',
-                'label' => '詳細'
-            ]
-        ];
-        $message = [
-            "to" => [$userId],
-            "type" => "multiple",
-            "altText" =>  "新着情報",
-            "contents" => $contents
-        ];
-        $data = json_encode([$message], JSON_UNESCAPED_UNICODE);
+        error_log("pushImportantINfo...");
+        $allMessages = []; //最後に使う
+
+        // 今日のデータのみにする？
+        date_default_timezone_set('Asia/Tokyo');
+        $today = date("Y-m-d");
+
+        // 全学部
+        $allStudents = DB::table('students')->select('user_id')
+            ->where('push_important', true)->get();
+        $allStudentsId = [];
+        foreach ($allStudents as $allStudent) {
+            $allStudentsId[] = $allStudent->user_id;
+        }
+
+        $allImportantInfomationsContents = [];
+        $allImportantInfomations = DB::table('informations')
+            ->join('tags', 'informations.id', '=', 'tags.information_id')
+            ->where('important', true)
+            ->orderBy('posted_date', 'desc')->limit(10)->get();
+        if ($allImportantInfomations->isEmpty()) {
+            $message = [
+                "to" => $allStudentsId,
+                "type" => "text",
+                "text" => "重要情報はありません",
+            ];
+        } else {
+            foreach ($allImportantInfomations as $allImportantInfomation) {
+                $allImportantInfomationsContent = [
+                    'title' => mb_substr($allImportantInfomation->title, 0, 40),
+                    'content' => mb_substr($allImportantInfomation->content, 0, 60),
+                    'uri' => $allImportantInfomation->uri,
+                    'label' => '詳細'
+                ];
+                $allImportantInfomationsContents[] = $allImportantInfomationsContent;
+            }
+            $message = [
+                "to" => $allStudentsId,
+                "type" => "multiple",
+                "altText" =>  "重要情報",
+                "contents" => $allImportantInfomationsContents
+            ];
+        }
+        $allMessages[] = $message;
+
+        $data = json_encode($allMessages, JSON_UNESCAPED_UNICODE);
+        error_log($data);
         $options = array(
             'http' => array(
                 'method' => 'POST',
@@ -88,12 +96,7 @@ class SampleCommand extends Command
             )
         );
         error_log(json_encode($data, JSON_UNESCAPED_UNICODE));
-
         $context = stream_context_create($options);
         $response = file_get_contents('https://tut-line-bot-test.glitch.me/push', false, $context);
-        echo gettype($response);
-        echo $response;
-
-        // return 0;
     }
 }
