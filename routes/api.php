510e2456d5aa16a642a39bccbcdebff8e0b00a0b
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-23 21:27:34 +0900

    done

--- routes/api.php
+++ routes/api.php
@@ -25,9 +25,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware(['throttle:testLimit'])->group(function () {
     Route::post('/v1/messages/reply', [ResponseController::class, 'response']);
+    Route::post('/parrot', [LineBotController::class, 'parrot']);
 });
-
-Route::post('/parrot', [LineBotController::class, 'parrot']);
 
 
 Route::get('/v1/settings/{id?}', [SettingController::class, 'index']);
