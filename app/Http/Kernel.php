f6cb7ce71e52e9502027c4d8abfbec4cd8926fae
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-23 19:53:02 +0900

    done

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,10 +40,10 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-        'api' => [
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        // 'api' => [
+        //     'throttle:api',
+        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        // ],
     ];
 
     /**
