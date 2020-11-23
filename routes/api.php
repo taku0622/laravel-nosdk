eb3475e608111d9203c64008ded8cdeded6f275c
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-23 21:08:15 +0900

    done

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/v1/messages/reply', [ResponseController::class, 'response']);
+Route::middleware(['throttle:api'])->group(function () {
+    Route::post('/v1/messages/reply', [ResponseController::class, 'response']);
+});
 
 Route::post('/parrot', [LineBotController::class, 'parrot']);
 
