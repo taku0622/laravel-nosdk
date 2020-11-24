2ddf9af25cc4ac6f4ffd59b2900605766e05a929
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-24 22:58:19 +0900

    done

--- app/Http/Controllers/DataBaseController.php
+++ app/Http/Controllers/DataBaseController.php
@@ -75,15 +75,6 @@ class DataBaseController extends Controller
         error_log("input: " . $inputs);
         $inputs = json_decode($inputs, true);
         foreach ($inputs as $input) {
-            error_log("input[date]: " . $input["date"]); // 1
-            error_log("input[title]: " . $input["title"]); // 2
-            error_log("input[instructor]: " . $input["instructor"]); // 3
-            error_log("input[department]: " . $input["department"]); // 4
-            error_log("input[grade]: " . $input["grade"]); // 5
-            error_log("input[class]: " . $input["class"]); // 5
-            error_log("input[note]: " . $input["note"]); // 6
-            error_log("input[up]: " . $input["up"]); // 7
-            error_log("input[from]: " . $input["from"]); // 8
             error_log("データベースに入れるデータに変換###################################");
             // データ整形
             $date = mb_substr($input["date"], 0, 10); // 2020年11月29
@@ -114,16 +105,6 @@ class DataBaseController extends Controller
                     $department = 'es';
                     break;
             }
-            error_log("date:" . $date); // 1
-            error_log("period:" . $period); // 2
-            error_log("lecture_name:" . $input["title"]); // 3
-            error_log("teacher_name:" . $input["instructor"]); // 4
-            error_log("grade:" . $input["grade"]); // 5
-            error_log("department:" . $department); // 6
-            error_log("class:" . $input["class"]); // 7
-            error_log("note: " . $input["note"]); // 8
-            error_log("posted_date: " . $posted_date); // 9
-            error_log("contributor: " . $input["from"]); // 10
             // データがない場合
             if (DB::table('cancel_informations')->where([
                 ['lecture_name', $input["title"]],
@@ -152,9 +133,6 @@ class DataBaseController extends Controller
         error_log("input: " . $inputs);
         $inputs = json_decode($inputs, true);
         foreach ($inputs as $input) {
-            error_log("input[name]: " . $input["title"]); // 1
-            error_log("input[instructor]: " . $input["instructor"]); // 2
-            error_log("input[reference]: " . $input["Reference"]); // 3
             DB::table('reference_informations')->updateOrInsert(
                 ['lecture_name' => $input["title"], 'teacher_name' => $input["instructor"]],
                 ['reference_name' => $input["Reference"]]
