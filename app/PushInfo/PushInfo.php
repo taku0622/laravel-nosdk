552adb3d69062ebe5785db23646561836d6a3964
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-26 17:03:55 +0900

    done

--- app/PushInfo/PushInfo.php
+++ app/PushInfo/PushInfo.php
@@ -88,7 +88,7 @@ class PushInfo
             if ($contents == []) {
                 continue;
             }
-            /////////////////////
+            // 10個に制限
             $contents = array_slice($contents, 0, 10);
             $message = [
                 "to" => [$student->user_id],
@@ -113,88 +113,5 @@ class PushInfo
         error_log(json_encode($data, JSON_UNESCAPED_UNICODE));
         $context = stream_context_create($options);
         $response = file_get_contents('https://tut-line-bot-test.glitch.me/push', false, $context);
-        //     error_log(json_encode($infoList));
-        //     $allMessages = [];
-        //     $allDepartmentContents = [];
-        // [[[all_department, cs, ms, bs], 5]]
-        //     foreach ($infoList as $info) {
-        //         // all_departmentが含まれる
-        //         if (in_array('all_department', $info[0])) {
-        //             error_log("##################### pushNew all_department ##################");
-        //             error_log("id: " . $info[1]);
-        //             error_log(gettype($info[1]));
-        //             $infomation = DB::table('informations')->where('id', $info[1])->first();
-        //             // error_log(json_encode($infomation), JSON_UNESCAPED_UNICODE);
-        //             error_log("title: " . $infomation->title);
-        //             error_log("content: " . $infomation->content);
-        //             $title4digit = mb_substr($infomation->title, 0, 4);
-        //             $title = $title4digit != "【新着】"  ? "【新着】" . $infomation->title : $infomation->title;
-        //             $text = $infomation->content == ''  ? '「詳細」を押してご確認ください。' : $infomation->content;
-
-        //             error_log($title);
-        //             error_log($text);
-
-        //             $content = [
-        //                 'title' => mb_substr($title, 0, 40),
-        //                 'content' => mb_substr($text, 0, 60),
-        //                 'uri' => $infomation->uri,
-        //                 'label' => '詳細'
-        //             ];
-        //             $allDepartmentContents[] = $content;
-        //         } else { // 各学部
-        //             $allStudentId = [];
-        //             $allStudentId = DB::table('students')
-        //                 ->where('push_new', true)->whereIn('department', $info[0])
-        //                 ->pluck('user_id');
-        //             error_log("allstudentid: " . $allStudentId);
-        //             $infomation = DB::table('informations')
-        //                 ->where('id', $info[1])->first();
-        //             $title4digit = mb_substr($infomation->title, 0, 4);
-        //             $title = $title4digit != "【新着】"  ? "【新着】" . $infomation->title : $infomation->title;
-        //             $text = $infomation->content == ''  ? '「詳細」を押してご確認ください。' : $infomation->content;
-        //             $content = [
-        //                 'title' => mb_substr($title, 0, 40),
-        //                 'content' => mb_substr($text, 0, 60),
-        //                 'uri' => $infomation->uri,
-        //                 'label' => '詳細'
-        //             ];
-        //             $message = [
-        //                 "to" => $allStudentId,
-        //                 "type" => "multiple",
-        //                 "altText" =>  "新着情報",
-        //                 "contents" => [$content]
-        //             ];
-        //             $allMessages[] = $message;
-        //         }
-        //     }
-        //     // allDepartmentContents
-        //     if ($allDepartmentContents != []) {
-        //         $allStudentId = DB::table('students')->where('push_important', true)->pluck('user_id');
-        //         error_log(json_encode($allStudentId), JSON_UNESCAPED_UNICODE);
-        //         $message = [
-        //             "to" => $allStudentId,
-        //             "type" => "multiple",
-        //             "altText" =>  "新着情報",
-        //             "contents" => $allDepartmentContents
-        //         ];
-        //         $allMessages[] = $message;
-        //     }
-        //     // 要素を切る
-        //     $allMessages = array_slice($allMessages, 0, 10);
-        //     // post
-        //     $data = json_encode($allMessages, JSON_UNESCAPED_UNICODE);
-        //     error_log($data);
-        //     $options = array(
-        //         'http' => array(
-        //             'method' => 'POST',
-        //             'header' => "Content-type: text/plain\n"
-        //                 . "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36\r\n" // 適当に名乗ったりできます
-        //                 . "Content-Length: " . strlen($data) . "\r\n",
-        //             'content' => $data
-        //         )
-        //     );
-        //     error_log(json_encode($data, JSON_UNESCAPED_UNICODE));
-        //     $context = stream_context_create($options);
-        //     $response = file_get_contents('https://tut-line-bot-test.glitch.me/push', false, $context);
     }
 }
