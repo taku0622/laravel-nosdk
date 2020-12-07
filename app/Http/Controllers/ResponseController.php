c2e97889e8d64e0a71ec6f6d9c80b393ac414ece
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-12-08 03:36:39 +0900

    done

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -114,7 +114,7 @@ class ResponseController extends Controller
         $uriList = DB::table('informations')->select('informations.uri')
             ->join('tags', 'informations.id', '=', 'tags.information_id')
             ->where('important', true)->groupBy('informations.uri')
-            ->orderBy('informations.id', 'desc')->limit(10)->get();
+            ->orderByRaw('max(informations.id) desc')->limit(10)->get();
         if ($uriList->isEmpty()) {
             $message = [
                 "to" => [$userId],
